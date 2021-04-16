aa6b4a46c432f791ea5a959e706818edb9b6b77e
swheo <vereurer@gmail.com>
2021-04-16 20:02:14 +0900

    imutils.py : scipy.msic imreisze to skimage.transform resize smpl.py : change smplx.body_models ModelOutput to SMPLOutput as ModelOutput

---
+++ run_demo.sh
@@ -0,0 +1,3 @@
+#Script for running demo.py
+
+python3 demo.py --checkpoint=data/model_checkpoint.pt --img=examples/im1010.jpg --openpose=examples/im1010_openpose.json
--- utils/imutils.py
+++ utils/imutils.py
@@ -5,6 +5,7 @@ import torch
 import numpy as np
 import scipy.misc
 import cv2
+from skimage.transform import resize
 
 import constants
 
@@ -76,7 +77,8 @@ def crop(img, center, scale, res, rot=0):
         new_img = scipy.misc.imrotate(new_img, rot)
         new_img = new_img[pad:-pad, pad:-pad]
 
-    new_img = scipy.misc.imresize(new_img, res)
+    # new_img = scipy.misc.imresize(new_img, res)
+    new_img = resize(new_img, res)
     return new_img
 
 def uncrop(img, center, scale, orig_shape, rot=0, is_rgb=True):
